bff40b264cf9b65dbf3ac0740fb6b8d9986be09f
akshay <akshay.kumar@keyvertex.com>
2020-05-29 13:28:15 +0530

    basic public code

--- wisdom-pet-medicine/src/index.js
+++ wisdom-pet-medicine/src/index.js
@@ -1,22 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './css/index.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import App from './component/App';
+import './css/index.css';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import 'jquery/dist/jquery.js';
 import 'popper.js/dist/umd/popper.js';
-import 'bootstrap/dist/js/bootstrap.js';  
+import 'bootstrap/dist/js/bootstrap.js';
 
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
