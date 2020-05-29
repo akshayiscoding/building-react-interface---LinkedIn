ce7427fc81de63c4ddb5110a82a23ecd959dca3e
akshay <akshay.kumar@keyvertex.com>
2020-05-29 13:02:28 +0530

    added modules

--- wisdom-pet-medicine/src/index.js
+++ wisdom-pet-medicine/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import App from './component/App';
 import * as serviceWorker from './serviceWorker';
+import 'jquery/dist/jquery.js';
+import 'popper.js/dist/umd/popper.js';
+import 'bootstrap/dist/js/bootstrap.js';  
+
 
 ReactDOM.render(
   <React.StrictMode>
